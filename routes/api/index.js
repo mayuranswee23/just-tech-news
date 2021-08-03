f5a0536b34372ca80d8fa9a849dbe8218a17da5b
Mayuran <mayuranswee23@gmail.com>
2021-08-03 00:43:44 -0400

    added comment methods

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-route');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-
+router.use('/comment', commentRoutes);
 
 
 
